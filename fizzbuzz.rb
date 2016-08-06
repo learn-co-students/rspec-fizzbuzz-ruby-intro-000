d85a359eca25c6a4d81de493daa8213105d497f2
Tal Masica <talmasica@gmail.com>
2016-08-06 17:17:15 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,11 @@
-require_relative './fizzbuzz_spec.rb'
+require_relative './spec/fizzbuzz_spec.rb'
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go Fizz
-  end
-  elsif
-     int % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go Buzz
-  elsif
-     (int % 3 == 0 && int % 5 == 0) # if the number int is divisible by 3 and 5
+  if (int % 3 == 0 && int % 5 == 0) # if the number int is divisible by 3 and 5
     "FizzBuzz" # Go FizzBuzz
-  elsif
-     (int % 3 == 0 || int % 3 == 0)
-     "nil" # if the number int is not divisible by 3 or 5
+  elsif int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go Fizz
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" # Go Buzz
   end
 end
