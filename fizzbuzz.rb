d40dec2fea3346de9cfd435862557036483c845f
Armaan Dhanji <dhanjiarmaan@gmail.com>
2016-08-11 15:45:12 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz (n)
-  if (n % 3 == 0) && (n % 5 == 0)
+  if (n % 5 == 0) && (n % 3 == 0)
     return "FizzBuzz"
   elsif (n % 5 == 0)
     return "Buzz"
-  elsif (n % 3 == 0)
-    return "Fizz"
+      elsif (n % 3 == 0)
+        return "Fizz"
   else
     return nil
   end
