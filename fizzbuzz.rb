3939061410c1bf2cd5e0fbc56a5c3c172bf4d566
Alejandro Coca <amcocarojas@gmail.com>
2016-08-19 17:08:50 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,17 +6,7 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
-  # else
-  #    return nil
+  else
+    return nil
   end
 end
-
-a = fizzbuzz(3)
-b = fizzbuzz(5)
-c = fizzbuzz(15)
-d = fizzbuzz(4)
-
-puts a
-puts b
-puts c
-puts d
