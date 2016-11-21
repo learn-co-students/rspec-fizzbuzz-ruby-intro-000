8fbde67c3ee5c01038adb5b88e5a5f2e7b9fa590
Dani Evans <daniellenevans@gmail.com>
2016-11-21 19:16:51 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3 == 0 
+    return "Fizz"
+  end
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
