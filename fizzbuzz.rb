533dff914f7070094eeaa59c261c9f34b139a982
Jeffrey Scott <jscott413@gmail.com>
2017-03-05 01:39:28 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  end
+  if int % 3 != 0 || int % 5 != 0
+    return
+  end
+end
