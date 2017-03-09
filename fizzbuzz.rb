dabc9ee2abd652c10504d4048b5dd21da860ae40
Bruno Ricard <bricard8@gmail.com>
2017-03-09 02:04:27 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+
+def fizzbuzz(int)
+  if int%3 == 0 && int%5 == 0
+    puts "FizzBuzz"
+    "FizzBuzz"
+ elsif int%5 == 0
+   puts "Buzz"
+   "Buzz"
+ elsif int%3 == 0
+  puts "Fizz"
+  "Fizz"
+  else
+    puts int
+    nil
+  end
+end
