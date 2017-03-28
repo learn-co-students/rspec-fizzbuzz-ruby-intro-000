700820ed4a76965228ab44d172f837313d15eb48
aaron wilswe <element1028@gmail.com>
2017-03-28 19:22:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,19 @@
 def fizzbuzz(num)
-  i = 1
-  while num >= i
-    if i % 3 == 0 && i % 5 == 0
-      puts "FizzBuzz"
-    elsif i % 5 == 0
-      puts "Buzz"
-    elsif i % 3 == 0
-      puts "Fizz"
-    else
-      nil
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+    "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+    "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+    "Buzz"
+  else
+    puts "Not divided by 3 or 5"
+    nil
   end
-  i += 1
-end
 end
 fizzbuzz(3)
 fizzbuzz(5)
 fizzbuzz(15)
+fizzbuzz(4)
