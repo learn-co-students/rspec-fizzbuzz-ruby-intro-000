ef47650c71554109cfc1dcf819374531f08d851e
aaron wilswe <element1028@gmail.com>
2017-03-28 17:34:20 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,18 @@
 def fizzbuzz(num)
-  num.times do |i|
-    if i % 3 == 0
-      puts "Fizz"
+  i = 1
+  while num >= i
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
     elsif i % 5 == 0
       puts "Buzz"
-    elsif (i % 3 == 0) && (i % 5 == 0)
-      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
     else
       nil
   end
+  i += 1
 end
 end
-fizzbuzz(21)
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
