3f511800b7e3f8f9fd1c83640333e68455d9bd57
Folu Obatusin <nisutabo@gmail.com>
2017-04-19 04:27:21 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+int = " "
+def fizzbuzz(int)
+if int % 5 == 0 && int % 3 == 0
+    "FizzBuzz"
+elsif int % 3 == 0
+    "Fizz"
+elsif int % 5 == 0
+    "Buzz"
+else
+    puts " "
+
+end
+
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
+require './fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
