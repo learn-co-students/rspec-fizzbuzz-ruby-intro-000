03df25154f76f9a431f9f63325872dfc1001c915
Isabelle Letaconnoux <imletaconnoux@gmail.com>
2017-05-22 17:07:05 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    return "FizzBuzz"
+  end
+ if int % 3 == 0
+   return "Fizz"
+ end
+ if int % 5 == 0
+  return "Buzz"
+ end
+ if int % 4 == 0
+  puts "nil"
+ end
+ end
