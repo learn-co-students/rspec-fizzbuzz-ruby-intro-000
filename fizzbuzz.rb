845e4867869c3711adfdde15f34c63faca930dd1
Gabriela D'Avila <me@gabriela.io>
2017-05-28 00:57:39 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(number)
+    if number % 15 == 0
+        "FizzBuzz"
+    elsif number % 5 == 0
+        "Buzz"
+    elsif number % 3 == 0
+        "Fizz"
+    else
+        nil
+    end
+end
