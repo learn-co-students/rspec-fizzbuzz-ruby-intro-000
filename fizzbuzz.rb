2e4e0669fe2e8e4bcf41919c447b9b8b89c9a801
Roman Johnson <roman.johnson@gmail.com>
2017-06-02 18:27:36 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+def fizzbuzz(number)
+  
+if 
+  number % 15 == 0
+  "FizzBuzz"
+
+elsif 
+  number % 5 == 0
+  "Buzz"
+elsif 
+  number % 3 == 0
+  "Fizz"
+end
+end 
+
+def is_divisible_by(number, divisor)
+  if number % divisor == 0
+  true 
+else false
+  end
+end
