322ae650c57aafc81285bd5cdaf52b96d2cdb69b
Sarah Wooten <snwooten@gmail.com>
2017-07-25 00:11:40 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz(int)
+
+    if int % 15 == 0
+    "FizzBuzz"
+
+    elsif int % 5 == 0
+      "Buzz"
+
+    elsif int % 3 ==0
+      "Fizz"
+
+    elsif int % 5 != 0
+
+
+    else int % 3 != 0
+  
+
+    end
+  end
