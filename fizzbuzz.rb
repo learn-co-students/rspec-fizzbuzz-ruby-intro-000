59cbae7f703114119e0d9e08b622f6d5b425ac3a
Greg Potter <gpnyc2003@yahoo.com>
2017-09-11 18:06:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,15 @@ def fizzbuzz
 
 end
 
+
+
 def fizzbuzz(int)
-    if int % 3 == 0
-        "Fizz"
-    end
-    if int % 5 == 0
-        "Buzz"
-    end
+if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0
+      "Fizz"
   elsif int % 5 == 0
-  "Buzz"
+      "Buzz"
+  
+end
 end
