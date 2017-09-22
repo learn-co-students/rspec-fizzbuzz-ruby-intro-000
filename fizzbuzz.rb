c07d1e4be1366772d433c8c2f2282adbb8685ac9
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-22 13:30:44 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+def fizzbuzz(int)
+  if int % 5 == 0 && int % 3 == 0
+    "FizzBuzz"
+  elsif
+     int % 3  == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+    end
+end
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
