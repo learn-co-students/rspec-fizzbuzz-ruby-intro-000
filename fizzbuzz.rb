74b985ccd11a1d719cc74d73cd56e077addcaeff
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-09-28 21:51:55 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,31 @@
+def fizzbuzz(number)
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif fizz_3 = number % 3 == 0
+    "Fizz"
+  elsif fizz_4 = number % 4 == 0
+    nil  
+  else fizz_5 = number % 5 == 0
+  "Buzz"
+
+end
+end
+
+
+
+#def fizzbuzz(int)
+  #fizz_3 = int % 3 == 0
+  #"Fizz"
+  #fizz_5 = int % 5 == 0
+  #"Buzz"
+  #fizz_15 = int % 15 == 0
+  #"FizzBuzz"
+#end
+
+
+#fizz_5 = int % 5 == 0
+#"Buzz"
+#fizz_15 = int % 3 == 0 and int % 5 == 0
+#{}"FizzBuzz"
+#fizz_4 = int % 4 == 0
+# " "
