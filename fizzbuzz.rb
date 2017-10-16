0a146f29eb0ba0dfd72cd85684c403cda8633863
Nick Wohnhas <npw7887@gmail.com>
2017-10-16 21:05:38 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(number)
+
+if number % 3 == 0 && number % 5 == 0
+   "FizzBuzz"
+elsif number % 5 == 0 && number % 3 != 0
+  "Buzz"
+elsif number % 3 == 0 && number % 5 != 0
+  "Fizz"
+else
+
+
+
+end
+
+
+
+end
