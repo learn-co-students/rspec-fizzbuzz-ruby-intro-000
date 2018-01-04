bf0d43602bad893758953c298069c5b45e9e8ac9
Tom Storm <tstorm@invoca.com>
2018-01-04 23:52:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif int % 3 == 0
-    puts "Fizz"
+    "Fizz"
   else
-    return nil
+    nil
   end
 end
