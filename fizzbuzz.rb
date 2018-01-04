a6a663bfae111476d068f243e3505412f1c9770e
Tom Storm <tstorm@invoca.com>
2018-01-04 23:38:27 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+def fizzbuzz(int)
+  if int % 15 == 0
+    puts "FizzBuzz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  else
+    return nil
+  end
+end
