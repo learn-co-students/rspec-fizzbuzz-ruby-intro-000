2d4ed5f94b39ae93cade604b8e5fab1ba1876342
Christopher Volpe <christopher.volpe12@gmail.com>
2018-12-07 22:52:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+    if int % 5 == 0
+      "FizzBuzz"
+    else "Fizz"
+    end
+  elsif int % 5 == 0
+    "Buzz"
+  else
+
+  end
+end
