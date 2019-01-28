5a04222ff7fc4a95e86f41423eb1be141c8188a1
Alicia Wu <ali.ciawu890@gmail.com>
2019-01-28 16:14:19 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
+  if (int % 3 == 0 && int % 5 == 0)
     "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
