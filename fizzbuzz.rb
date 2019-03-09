9195af5dc48c31ca459ec71e4ac4abe4b1ca325d
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-09 04:56:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if (number % 5 == 0) && (number % 3 == 0)
+  if number % 15 == 0 #(number % 3 == 0) && (number % 5 == 0)
     "FizzBuzz"
   elsif number % 5 == 0
     "Buzz"
   elsif number % 3 == 0
-   "Fizz"
+    "Fizz"
   end
 end
