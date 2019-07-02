606e2a8e74ccb53dcda8a675f22578d694ada081
Will Hodgson <will.hodgson@current-rms.com>
2019-07-02 15:11:34 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(val)
+  if val % 5 == 0 && val % 3 == 0
+    "FizzBuzz"
+  elsif val % 5 == 0
+    "Buzz"
+  elsif val % 3 == 0
+    "Fizz"
+  end
+end
