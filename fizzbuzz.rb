77bfb9c11312db700befd47ea201a632f6fc04a1
Tiffany Tang <tifftang22@gmail.com>
2019-07-06 05:21:19 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 != 0)
-    a = "Fizz"
+    return "Fizz"
   elsif (int % 5 == 0) && (int % 3 != 0)
-    b = "Buzz"
+    return "Buzz"
   elsif (int % 3 == 0) && (int % 5 == 0)
-    c = "FizzBuzz"
+    return "FizzBuzz"
   else
-    d = nil
+    return nil
   end
 end
