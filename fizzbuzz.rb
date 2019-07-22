e7e019996addc905c9d07d0063e14d32eb244c6e
Gordon Gyakyi <gordon.gyakyi@btinternet.com>
2019-07-22 20:02:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+fizz_3 = "Fizz"
+fizz_5 = "Buzz"
+fizz_15 = "FizzBuzz"
+
+if int % 3 == 0 && int % 5 == 0
+  return fizz_15
+
+elsif int % 3 == 0
+  return fizz_3
+
+elsif int % 5 == 0
+  return fizz_5
+
+end
+
+end
