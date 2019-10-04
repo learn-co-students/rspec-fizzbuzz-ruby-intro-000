28981e256e6e945cf1a529d05164a46edb929ea5
John Fazekas <john.a.fazekas@gmail.com>
2019-10-04 02:04:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(i)
+
+  output = ""
+
+  if(i % 3 == 0)
+    output = output + "Fizz"
+  end
+
+  if(i % 5 == 0)
+    output = output + "Buzz"
+  end
+
+  return output == "" ? nil : output
+end
