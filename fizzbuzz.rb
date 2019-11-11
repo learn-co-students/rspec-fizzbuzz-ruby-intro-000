fae918388466ddaf1792571e577f9224848862ee
Luke Lin <lukelin1991@yahoo.com>
2019-11-11 01:20:15 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && !(int % 5 == 0)
+    "Fizz"
+  elsif int % 5 == 0 && !(int % 3 == 0)
+    "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  else
+  end
+end
+
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
