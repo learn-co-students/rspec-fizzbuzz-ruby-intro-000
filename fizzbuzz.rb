faa40c58bee4c4885ec53ac5645ac228c23d866c
stellar-class-3593 <csamanta95@gmail.com>
2019-11-25 18:05:49 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 5 == 0 && num % 3 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num =! 5 && num =! 3
+    "nil"
+  end
+end
