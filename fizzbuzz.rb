c4b8d0f67ab7fa5d9014b088a52a9e19213330e4
Bryant Omoregie <unclebryant@gmail.com>
2020-01-17 04:19:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
-def fizzbuzz(5)
-  if int % 5 == 0
-    puts "Buzz"
+def fizzbuzz(bry)
+  if bry % 15 == 0
+    return "FizzBuzz"
+  elsif bry % 5 == 0
+    return "Buzz"
+  elsif bry % 3 == 0
+    return "Fizz"
+  else
+    puts nil
+
+
+
   end# Don't forget! This file needs to be 'required' in its spec file
 end
