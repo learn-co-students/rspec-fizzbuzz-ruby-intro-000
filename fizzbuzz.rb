b942c56404f48fc514b876741bf15985fc479323
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 04:49:12 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,49 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+require_relative './spec/fizzbuzz_spec.rb'
+
+=begin
+puts "Find a number that will get you to FIZZBUZZ."
+
+def fizzbuzz(int)
+  
+  if int % 3  
+    puts "Fizz"
+  
+  elsif int % 5  
+    puts "Buzz"
+  
+  elsif int % 15 
+    puts "FizzBuzz"
+  
+  else
+    puts "nil"
+ 
+ end
+end
+
+
+fizzbuzz(3)
+=end
+
+
+
+
+puts('Lets play fizzbuzz')
+print('enter a number: ')
+number = gets()
+puts(number)
+if number == % 3
+  puts ('fizz')
+elsif number == % 5
+  puts ('buzz')
+elsif number == %15
+  puts ('fizzbuzz')
+end
+
+
+
+
+
+
+
+
+
