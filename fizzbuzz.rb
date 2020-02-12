bc0a0e2efe7e8134533e6f3780e93e6e88777c40
Bashir <bashiralhanshali@yahoo.com>
2020-02-12 02:41:03 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  word='';
+  if int % 3 == 0
+    word = 'Fizz'
+  end
+  if int%5 == 0
+    word = word + 'Buzz'
+  end
+  if word == ''
+    word = nil
+  end
+  return word
+end
