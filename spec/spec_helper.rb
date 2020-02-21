80e474d19f96a8fec92c3a5f3988ea8b4f1bd565
Michaylah Mam <mchylh@gmail.com>
2020-02-21 19:32:06 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 # NOTE: If you uncomment the following line,
-# you won't encounter the `undefined method \`fizzbuzz'`
-# error described in the README
-# require_relative "../fizzbuzz"
+#you won't encounter the `undefined method \`fizzbuzz'`
+#error described in the README
+require_relative "../fizzbuzz"
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
