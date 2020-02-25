8dcf54d25edc7c2d94dcee50da79bf4d925c522d
Xi Chen <stephaniechen319@gmail.com>
2020-02-25 21:43:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(i)
+  if i % 3 == 0 && i % 5 == 0
+    "FizzBuzz"
+  elsif i % 3 == 0
+    "Fizz"
+  elsif i % 5 == 0
+    "Buzz"
+  end
+
+end
