cdc69ff4978b75792e3c98ea4571ebe3695899f7
Cathy D'Onofrio <catd825@gmail.com>
2020-03-09 21:35:23 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
-
-
-fizzbuzz_3=fizzbuzz(3)
-fizzbuzz_5=fizzbuzz(5)
-fizzbuzz_15=fizzbuzz(15)
 
 
 def fizzbuzz(int)
   if int % 15 == 0
-  puts "Fizzbuzz"
+   return "FizzBuzz"
   elsif int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
-  else
-    puts "na"
+    return "Buzz"
   end
 end
