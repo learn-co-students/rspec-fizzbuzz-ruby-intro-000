bd696ca42b4f34d94adf1436032e37b5565d2768
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-16 15:15:28 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 
+    return "FizzBuzz"
+  elsif
+    int % 3 == 0 # if the number int is divisible by 3
+    return "Fizz" # Go fizz
+   elsif int % 5 == 0
+   return "Buzz"
+end
+end 
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
