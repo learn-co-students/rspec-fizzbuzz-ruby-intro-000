afbe8c570b85bc2992270e373e6e441f755694f3
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-23 18:44:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(i)
+  if i % 3 == 0 && i % 5 == 0
+    return "FizzBuzz"
+  elsif i % 3 == 0
+    return "Fizz"
+  elsif i % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+end
+
+# fizzbuzz(3)
+# fizzbuzz(5)
+# fizzbuzz(15)
+# fizzbuzz(4)
