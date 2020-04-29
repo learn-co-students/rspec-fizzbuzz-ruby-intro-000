2938d8796b48825cc9ec214bc22ee6a289a1e8fc
Christine Ayoh <christineayoh9@gmail.com>
2020-04-29 04:05:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(x)
+  if (x % 5 == 0) && (x % 3 == 0)
+    puts "FizzBuzz"
+    return "FizzBuzz"
+  elsif x % 3 == 0
+    puts "Fizz"
+    return "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+    return "Buzz"
+
+  else
+    puts "NADA"
+    return nil
+end
+end
