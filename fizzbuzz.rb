660762bd5f5462d1550c43408e6505494140eda7
Luna Ribas <andrew.j.ribas@gmail.com>
2020-05-06 19:46:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  string = ""
+  if num % 3 == 0
+    string += "Fizz"
+  end
+  if num % 5 == 0
+    string += "Buzz"
+  end
+  if(num % 3 != 0 && num % 5 != 0)
+    return nil
+  end
+  string
+end
