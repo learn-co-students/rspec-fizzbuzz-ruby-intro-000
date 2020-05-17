1f90f910f31d9e3a514623f19538f7109b0dc8eb
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-17 14:46:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(userInput)
-if userInput % 3 = 0
-  puts "fizz"
-end elsif userInput % 5 = 0
-  puts "buzz"
-end elsif userInput % 3 = 0 && userInput % 5 = 0
-  puts "fizzbuzz"
-end
-end
+  if userInput % 3 == 0 && userInput % 5 != 0
+    "Fizz"
+  elsif userInput % 5 == 0 && userInput % 3 != 0
+     "Buzz"
+   elsif userInput % 3 == 0 && userInput % 5 == 0
+     "FizzBuzz"
+   else
+      nil
+   end
+ end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper.rb'
-require_relative './fizzbuzz.rb'
+require_relative '../fizzbuzz.rb'
+
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation!
 
 describe "fizzbuzz" do
