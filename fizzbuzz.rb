9c7164b6d78e9d6fa2035b1c16985a844cdd3b51
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-18 00:05:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,22 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0
+  "Fizz"
+  end
+if int % 5 == 0
+  "Buzz"
+  end
+if int % 3 && 5
+  "FizzBuzz"
+  end
+if int % 3 == nil || int % 5 == nil
+  "nil"
+  end
+
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
