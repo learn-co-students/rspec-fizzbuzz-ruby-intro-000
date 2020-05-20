e56d7abd8dae69c2fe6069e16ec1294993f31acf
Marc C <m.ciborowski@hotmail.com>
2020-05-20 02:44:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
-def fizzbuzz(int)
-  gets.chomp = int .to_i
-  if int % 3 == 0
-  puts "Fizz"
-elsif if int % 5 == 1
-  puts "Buzz"
-elsif int % 15 == 1
-  puts "FizzBuzz"
-else int % 4 == 0
-  puts nil
+def fizzbuzz(number)
+if (number % 15 == 0) && (number % 3 ==0)
+  "FizzBuzz"
+elsif number % 5 == 0
+  "Buzz"
+elsif number % 3 == 0
+  "Fizz"
+  end
 end
