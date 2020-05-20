da18a92616efb6a00deab9935ddb5d733b553261
Marc C <m.ciborowski@hotmail.com>
2020-05-20 00:50:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(int)
+  gets.chomp = int .to_i
+  if int % 3 == 0
+  puts "Fizz"
+elsif if int % 5 == 1
+  puts "Buzz"
+elsif int % 15 == 1
+  puts "FizzBuzz"
+else int % 4 == 0
+  puts nil
+end
