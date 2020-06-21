fa03d94ebf23039e9acfea7a9af8fe4a16471752
groana <contact@groanamelendez.com>
2020-06-21 17:27:48 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,13 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
     "FizzBuzz"
   elsif num % 3 == 0
+    puts "Fizz"
     "Fizz"
   elsif num % 5 == 0
+    puts "Buzz"
     "Buzz"
   end
 end
