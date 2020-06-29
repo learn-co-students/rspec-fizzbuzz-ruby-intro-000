4dc218bfe2b258487ec37924b115ed4ec682b556
Deja Monae Allison <dejaallison@gmail.com>
2020-06-29 15:07:37 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
-  end
-  if int % 5 == 0
-    puts "Buzz"
-  end
-  if int % 3 & 5 == 0
-    puts "fizzbuzz"
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
   end
 end
