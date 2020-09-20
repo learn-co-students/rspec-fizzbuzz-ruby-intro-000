5cadaaa4a9ab3060f6c6d42791c4ab90ab5de5c5
William Stoneberg <wrstoneberg@me.com>
2020-09-20 22:59:00 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+require 'pry'
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+    end
+end# Don't forget! This file needs to be 'required' in its spec file
+# See R EADME.md for instructions on how to do this
