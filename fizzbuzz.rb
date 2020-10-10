1020b5870adfbea2630c7a487bfcb43e796ee9bd
Josh Gumerove <josh.gumerove@gmail.com>
2020-10-10 20:59:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
+    "FizzBuzz" # Go fizz
+  elsif  int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+elsif int % 5 == 0 # if the number int is divisible by 5
+  "Buzz" # Go buzz
+else
+  nil # Go buzz  end
+end
+end
+
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz(15)  # => You should get an ArgumentError
