3f08aa506588764a972f3514dcca42532f21c69f
Victoria Alee <victoriaallee@gmail.com>
2020-11-25 04:19:11 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,4 +1,8 @@
+require_relative '../fizzbuzz.rb'
+
 require_relative './spec_helper.rb'
+
+
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
 describe "fizzbuzz" do
