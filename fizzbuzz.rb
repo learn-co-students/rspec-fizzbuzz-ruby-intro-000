5f8610e85113e374cc87b33a79bcb8ab6f8ede05
Anna <ajlukasik@gmail.com>
2021-01-29 19:25:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,24 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-   return "FizzBuzz"
+ #  if int % 3 == 0 && int % 5 == 0
+ #   return "FizzBuzz"
+ #  end
+ #
+ #  if int % 5 == 0
+ #   return "Buzz"
+ #  end
+ #
+ #  if int % 3 == 0
+ #   return "Fizz"
+ # end
+ if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
   end
-
-  if int % 5 == 0
-   return "Buzz"
-  end
-
-  if int % 3 == 0
-   return "Fizz"
- end
 end
 
 fizzbuzz(3) # => You should see a return of "Fizz"
