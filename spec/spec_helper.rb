d65590c02fa1ec82ba1280562134a2bcaf0dc432
Ryan Waggoner <ryan@waggoner.co>
2021-02-10 05:27:17 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,14 +8,10 @@
 # NOTE: If you uncomment the following line,
 # you won't encounter the `undefined method \`fizzbuzz'`
 # error described in the README
-# require_relative "../fizzbuzz"
+require_relative "../fizzbuzz"
+
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = 'default'
 end
