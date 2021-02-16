56bf1b11033095764874aad1910d09774f2510d1
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-16 19:00:25 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(input)
+  if input==3
+    return "Fizz"
+  elsif input==5
+    return "Buzz"
+  elsif input==15
+    return "FizzBuzz"
+  end
+end
