9b2328c2abaa24df94b844d12cbf4aff38ba50ef
James Daaran <jamesdaaran@gmail.com>
2021-04-01 13:54:13 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+#value = gets.strip
+#  input = value.to_i
+
+def fizzbuzz(input)
+fizz_3 = input % 3
+fizz_5 = input % 5
+
+if fizz_3 == fizz_5
+  "FizzBuzz"
+elsif fizz_3 == 0
+  "Fizz"
+elsif fizz_5 == 0
+  "Buzz"
+end
+end
